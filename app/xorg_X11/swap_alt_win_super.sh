9d3e5a353fc5d4383af69a0bdeeb391792f8c70d
thinkpad in arco linux <sunyta2@naver.com>
2019-10-16 21:37:18 +0900

    setxkbmap_CMD_alt twm to dwm change

---
+++ app/xorg_X11/swap_alt_win_super.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# saved in /etc/X11/xinit/xinitrc.d/swap_alt_win_super.sh _IN Arch xfce saved
+#/usr/bin/setxkbmap -option altwin:swap_alt_win # change win and alt in thinkpad when executed.
+# 이곳에 실행파일을 넣어두고서 상용으로 폼의 실행을 가능하게 하는 것을 고려해 봄직하다.
+
+# will saved in ~/config_github/app/xorg_X11/xinitrc_d_swap_alt_win_super_sh_in_arch_xfce_etc
+# (shell-command "cp /etc/X11/xinit/xinitrc.d/swap_alt_win_super.sh /home/thinkpad/config_github/app/xorg_X11/xinitrc_d_swap_alt_win_super_sh_in_arch_xfce_etc)
+
+# (shell-command "cp /etc/X11/xinit/xinitrc.d/swap_alt_win_super.sh ~/config_github/app/xorg_X11/xinitrc_d_swap_alt_win_super_sh_in_arch_xfce_etc)
