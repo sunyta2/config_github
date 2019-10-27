7d1e9a74b468ce73945db9e7a8bd915c461952bd
thinkpad in arco linux <sunyta2@naver.com>
2019-10-27 17:10:09 +0900

    dotspacemacs-install-packages 'used-but-keep-unused #package-install 유지됨의 묘책

--- app/emacs/shell_emacs/emacsQ_load.el
+++ app/emacs/shell_emacs/emacsQ_load.el
@@ -3,18 +3,49 @@
 ;; emacs -Q -nw --eval '(set-input-method "korean-hangul")' $@
 ;; emacs -Q -nw -l "~/config_github/app/emacs/shell_emacs/shell_command_emacs.org" $@
 
+;;설계도
+;1.OS의 출발;~/.bin/start == ~/config_github/start.sh와 동일할 것을 맞춤
+;2.emacsQ go;~/.bin/emacsQ == 
+;3.emacsQ load  <즉, 이파일>
 
-;; 영어로 다시 반환함  
 
 (progn
-  (set-input-method "korean-hangul")
-  (toggle-input-method)
-  (org-mode)
+  (org-mode) ;; 아젠다뷰를 넣을 것을 고려할 것.
   ;;(message "hi")
   ;(progn
   (dired-other-window "~/config_github/shell_script/")(dired-hide-details-mode)
   (bookmark-bmenu-list)
 )
+
+;; org-src possible by below
+
+;(require 'ob-sh)
+;(org-babel-do-load-languages
+; 'org-babel-load-languages '((sh . t)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default-input-method "korean-hangul")
+; (set-input-method "korean-hangul") ;; 바로 무력화됨 ;; 영어로 다시 반환함  
+; (toggle-input-method)              ;; 바로 무력화됨
+
+ '(org-babel-load-languages
+   (quote
+    ((emacs-lisp . t)
+     (shell . t)
+     (org . t)
+     (python . t)
+     (ruby . t)
+     (C . t)))))
+
+
+;; addtion of shortcut
+;; C-x C-j jump-dired
+                                   ;; C-g  g grep-mac-chrome
+
+
 
 (when (>= emacs-major-version 24)
   (require 'package)
@@ -29,10 +60,12 @@
 ;;;; 설치되어 있었음 (package-install "use-package")
 
 ;;[[file:~/config_github/app/emacs/melpa_package.org]]
+(add-to-list 'load-path "~/.emacs.d/emacsQ_use_package/")
 (package-initialize)
 (eval-when-compile
   ;; Following line is not needed if use-package.el is in ~/.emacs.d
-;;  (add-to-list 'load-path "<path where use-package is installed>")
+  ;;  (add-to-list 'load-path "<path where use-package is installed>")
+
   (require 'use-package))
 
 
