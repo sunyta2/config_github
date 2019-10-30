4289061b34918c6dfc7ad12c4a9c16be4ec9e9ac
thinkpad in arco linux <sunyta2@naver.com>
2019-10-30 12:27:41 +0900

    add stuff

--- app/emacs/shell_emacs/emacsQ_load.el
+++ app/emacs/shell_emacs/emacsQ_load.el
@@ -4,7 +4,7 @@
 ;; emacs -Q -nw -l "~/config_github/app/emacs/shell_emacs/shell_command_emacs.org" $@
 
 ;;설계도
-;1.OS의 출발;~/.bin/start == ~/config_github/start.sh와 동일할 것을 맞춤
+;1.OS의 출발;~/.bin/start == ~/config_github/start.sh와 동일할 것을 맞춤 (find-file "~/.bin/start")
 ;2.emacsQ go;~/.bin/emacsQ == 
 ;3.emacsQ load  <즉, 이파일>
 
@@ -43,6 +43,8 @@
 
 ;; addtion of shortcut
 ;; C-x C-j jump-dired
+;; C-x 4 C-j 가 유용해서 불필요함. (define-key global-map (kbd "C-x C-j") 'dired-jump)
+;;                 이후 C-x 4 0 로 사용을 중단함.
                                    ;; C-g  g grep-mac-chrome
 
 
@@ -84,6 +86,27 @@
 )
 
 
+;; Emacs as a C/C++ Editor/IDE (Part I) https://www.youtube.com/watch?v=HTUE03LnaXA
+; (package-initialize)
+ ; (package-refresh-contents)
+(use-package yasnippet
+	:ensure t
+        :config
+	(use-package yasnippet-snippets
+		 :ensure t)
+	;;(yas-reload-all))
+	(yas-global-mode 1))
+(use-package company)
+
+(use-package auto-complete)
+(use-package auto-complete-config)
+(ac-config-default)
+
+
+(use-package zeal-at-point) ;; 잘 작동하지는 않음
+
+;; (use-package iedit-mode)
+(define-key global-map (kbd "C-;") 'iedit-mode)
 
 (use-package simpleclip
 :config
